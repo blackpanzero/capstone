1bed22cf1ace56e8cf4a017ab1e02f95d3dc6a5b
Derrick Otieno <56361057+blackpanzero@users.noreply.github.com>
2020-08-30 16:00:03 +0300

    Update settings.py

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_on_heroku
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,5 +126,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Activate Django-Heroku.
-django_on_heroku.settings(locals())
-
+django_heroku.settings(locals())
